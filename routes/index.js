afc18f22db27737efc93355747c2cb38f8fbeeb1
chinnua <chinnu.a@gmail.com>
2018-03-30 11:22:23 +0530

    CORS

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ var TodoService = require('../services/todos.service')
 
 router.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
