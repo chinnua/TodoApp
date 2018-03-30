f890e13e2507c2e56600eb7c0f4de38c2cc1c150
chinnua <chinnu.a@gmail.com>
2018-03-30 10:50:51 +0530

    CORS

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,12 @@ var TodoService = require('../services/todos.service')
 //   res.render('index', { appTitle: 'Todo App', status: false, listTodos: JSON.stringify(todos) });
 // });
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 router.get('/', (_, res) => {
   var promise = TodoService.getTodos({}, 1, 100);
   promise.then(function (data) {
